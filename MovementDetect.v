42924137abe83d85e711413bed986220607aa1a2
Butcher <landonbu@UMICH.EDU>
2024-03-22 01:00:57 -0400

    All for tonight. We will likely have to pipeline and parallel everything we can.

--- MovementDetect.v
+++ MovementDetect.v
@@ -1,5 +1,13 @@
-module MovementDetect(one,two);
-	input one;
-	output two;
-	assign two = one;
+module MovementDetect
+	#(parameter pixels = 64)
+	#(parameter colorW = 16)
+	#(parameter numMax = 10)
+	(rColumn,gColumn,bColumn,DIFF);
+	input [pixles:0][colorW:0]rColumn,gColumn,bColumn;
+	output [colorW+numMax+2:0]DIFF;
+	
+	wire [pixles:0][colorW+2:0]diffs;
+	
+	compPixel comps[pixels:0] (rColumn,gColumn,bColumn,diffs);
+	
 endmodule 
